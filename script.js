5f177ffac2f74607a41b9d411f5da0dde521ed66
Luqman Hassan <reallookmanlook@gmail.com>
2021-07-10 17:51:28 -0700

    Update

---
+++ script.js
@@ -0,0 +1,13 @@
+function Todos(title, description, duedate, priority, notes, checklist) {
+    this.title = title;
+    this.description = description;
+    this.duedate = duedate;
+    this.priority = priority;
+    this.notes = notes;
+    this.checklist = checklist;
+}
+
+const ym = new Todos();
+Todos.prototype.yolo = "Bitch";
+
+console.log(ym.yolo);
