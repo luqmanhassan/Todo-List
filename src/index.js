5dfe9829a9266530c2acb36341e7b9bd695965b7
Luqman Hassan <reallookmanlook@gmail.com>
2021-08-22 20:56:31 -0700

    Modified logic to append todo on Today & week sections

--- src/index.js
+++ src/index.js
@@ -7,16 +7,24 @@ class Todos {
     this.notes = notes;
     this.checklist = checklist;
   }
-  /*
+
   makeproject() {
     let doc = document.getElementById('left');
     let div = document.createElement('div');
-    let divText = document.createTextNode(value);
     let value = document.getElementById('addProject_Input').value;
+    let ym = document.getElementById('addProject_btn');
+    let p1 = document.createElement('p');
+    let p2 = document.createElement('p');
+    p1.style.width = '100px';
+    let p1Text = document.createTextNode(value);
+    let p2Text = document.createTextNode('X');
     doc.insertBefore(div, ym);
-    div.appendChild(img);
+    p1.appendChild(p1Text);
+    p2.appendChild(p2Text);
+    div.appendChild(p1);
+    div.appendChild(p2);
   }
-  */
+
   maketask() {
     let doc = document.getElementById('display-inbox');
     let value = document.getElementById('addTask_Input').value;
@@ -26,11 +34,24 @@ class Todos {
     div.className = 'todo-div';
     let img = document.createElement('img');
     img.className = 'div-todo-img';
+    img.onclick = delete_task;
     let p1 = document.createElement('p');
     p1.style.width = '200px';
     let input = document.createElement('input');
     input.type = 'date';
+    input.onchange = dist;
+    // Place Todo Item on other Div Sections **********
+    function dist(event) {
+      let e = event.target.value;
+      let d = new Date(e).getUTCDate();
+      let c = new Date().getUTCDate();
 
+      if (d == c) {
+        console.log('Check');
+        document.getElementById('display-today').appendChild(div);
+        doc.insertBefore(div, ym);
+      }
+    }
     let p2 = document.createElement('p');
     p2.onclick = delete_task;
     function delete_task() {
@@ -57,6 +78,7 @@ document.getElementById('addTask_btn').addEventListener('click', displayTask);
 
 function displayTask() {
   document.getElementById('addTask_div').style.display = 'block';
+  document.getElementById('addTask_Input').value = '';
   this.style.display = 'none';
 }
 
@@ -82,8 +104,19 @@ document
 
 function displayProject() {
   document.getElementById('addProject_div').style.display = 'block';
+  document.getElementById('addProject_Input').value = '';
+
   this.style.display = 'none';
 }
+document
+  .getElementById('addProject_addbtn')
+  .addEventListener('click', function () {
+    document.getElementById('addProject_div').style.display = 'none';
+    document.getElementById('addProject_btn').style.display = 'block';
+    const todo = new Todos();
+    todo.makeproject();
+  });
+
 document
   .getElementById('addProject_cancelbtn')
   .addEventListener('click', function () {
